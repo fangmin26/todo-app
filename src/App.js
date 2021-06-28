10fe36c5673d269b8663d97a3145918d9ba5c399
fangmin26 <fangmin_@naver.com>
2021-06-28 16:45:13 +0900

    0628reactTODO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './component/TodoTemplate';
+import TodoInsert from './component/TodoInsert';
+import TodoList from './component/TodoList';
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checekd: false,
+    });
+  }
+  return array;
 }
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos((todos) => todos.concat(todo));
+      nextId.current += 1;
+    },
+    [],
+    //   setTodos(todos.concat(todo));
+    //   nextId.current += 1;
+    // },
+    // [todos],
+  );
+  const onRemove = useCallback(
+    (id) => {
+      setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    },
+    [],
+    //      setTodos(todos.filter((todo) => todo.id !== id));
+    //    },
+    //    [todos],
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setTodos((todos) =>
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [],
+    //   setTodos(
+    //     todos.map((todo) =>
+    //       todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    //     ),
+    //   );
+    // },
+    // [todos],
+  );
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
 
 export default App;
